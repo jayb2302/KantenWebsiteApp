898d6df1aff89476efcebae3a2c062a5e047ef7d
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2023-05-16 11:22:00 +0200

    aha

--- kanten-project/src/App.vue
+++ kanten-project/src/App.vue
@@ -6,6 +6,7 @@
       <RouterLink class="text-2xl mr-4 ml-4" to="/register"> Register </RouterLink>
       <RouterLink class="text-2xl mr-4 ml-4" to="/sign-in"> Login </RouterLink> 
       <RouterLink class="text-2xl mr-4 ml-4" to="/event"> Add Event </RouterLink> 
+      
       <RouterView/>
     </nav>
     
