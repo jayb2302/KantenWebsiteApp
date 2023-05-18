918c79c821f21ca67e1178fe4da25a3ca5f7afb9
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2023-05-18 11:30:56 +0200

    shop-frg

--- kanten-project/src/views/Shop.vue
+++ kanten-project/src/views/Shop.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-
-    </div>
+    <!-- Shop -->
+    <section class="shop container">
+        <h2 class="section-title">Shop Products</h2>
+    </section>
+    
 </template>
 
 <script setup>
@@ -9,7 +11,8 @@
 </script>
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,800;1,300&display=swap');
+@import './base.css';
 *{
     margin: 0;
     padding: 0;
@@ -20,4 +23,22 @@
     text-decoration: none;
 }
 
+:root {
+    --main-color: #252625;
+    --second-bg-color: #A020EA;
+    --k-green: #7FFB50;
+     --k-yellow: #FDE303;
+     --k-red: #DC4941;
+    --k-blue: #5ABFF9;
+     --k-pink: #EB4EBA;
+     --gray-text: #9b9da0;
+    --white-text: #c4c6c8;
+}
+
+img {
+    width: 100%;
+}
+body{
+    color: var(--main-color);
+}
 </style>
