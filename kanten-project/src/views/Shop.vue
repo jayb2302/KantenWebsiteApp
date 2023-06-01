5651afd091836a8ebf56b491e5c520edfb2c1f7e
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2023-06-01 13:17:58 +0200

    codes

--- kanten-project/src/views/Shop.vue
+++ kanten-project/src/views/Shop.vue
@@ -19,6 +19,65 @@
         <Cart/>
     </div>
   </div>
+
+  <!-- Footer -->
+<footer class="footer">
+        <div class="footer-content">
+          <div class="line">
+            
+  
+            <div class="footer-widget2">
+              <div class="footer-widget-heading">
+                <h3>Find us here</h3>
+              </div>
+              <ul>
+                <li>
+                  <h2>Kanten</h2>
+                  <a href="https://goo.gl/maps/FiNur8K3VDgcWFk39">FINSENSGADE 1, 6700 Esbjerg</a>
+                </li>
+                <li>
+                  <h2>Kraftværket</h2>
+                  <a href="https://goo.gl/maps/m9AhRALJE7bfsctq9">GL NOVRUPVEJ 14, 6705 ESBJERG Ø</a>
+                </li>
+              </ul>
+            </div>
+  
+            <div class="footer-widget2">
+              <div class="footer-widget-heading">
+                <h3>Contact us</h3>
+              </div>
+              <ul>
+                <li>
+                  <h2>Phone</h2>
+                  <p>+45 28 97 11 85</p>
+                </li>
+                <li>
+                  <h2>E-mail</h2>
+                  <p>MUSIKFORENINGENKANTEN@GMAIL.COM</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </footer>
+        <div class="copyright-area">
+                <div class="row">
+                        <div class="copyright-text">
+                            <p>Copyright &copy; 2023, All Right Reserved Kanten</p>
+                        </div>                    
+                </div>
+                <div class="footer-widget">
+              <div class="footer-logo">
+                <img src="../assets/img/Logo.png" class="img-fluid" alt="logo">
+              </div>
+              <!-- Follow button -->
+              <!-- <div class="footer-social-icon">
+                <span>Follow us</span>
+                <a href="#"><i class='bx bxl-facebook' style='color:#ffffff'></i></a>
+                <a href="#"><i class='bx bxl-instagram' style='color:#ffffff'  ></i></a>
+              </div> -->
+            </div>
+        </div>
 </template>
 
 <script >
@@ -246,5 +305,129 @@ export default {
   height: 100vh; /* Set the height to occupy the full screen */
   overflow-y: auto;
 }
+
+                                  /* Footer */
+
+
+.footer {
+  background: $primaryone;
+  padding-top: 70px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  height: 300px !important;
+}
+
+.footer-content {
+  width: 100%;
+  flex-grow: 1;
+}
+.line {
+  display: flex;
+  justify-content: center;
+}
+.footer-widget {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.footer-widget2 {
+  width: 100%;
+  max-width: 300px;
+  text-align: left;
+}
+.footer-widget-heading {
+  margin-bottom: 10px;
+  font-family: $brother;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  font-family: $k2d;
+}
+
+li {
+  margin-bottom: 10px;
+  
+}
+
+h2{
+    font-weight: bold;
+    font-size: large;
+}
+
+.footer-pattern img {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 330px;
+  background-size: cover;
+  background-position: 100% 100%;
+}
+
+.footer-logo img {
+    max-width: 200px;
+}
+ 
+.footer-widget-heading h3 {
+  color: $white;
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 40px;
+  position: relative;
+}
+.footer-widget-heading h3::before {
+  content: "";
+  position: absolute;
+  bottom: -7px;
+  height: 3px;
+  width: 116px;
+  background: $kpink;
+}
+
+.copyright-area{
+  background: $primaryone;
+  display: flex;
+  justify-content: center;
+  height: 50px !important;
+}
+.copyright-text p {
+  margin: 0;
+  font-size: 14px;
+  color: $white;
+}
+
+@media (max-width: 600px) {
+  .footer {
+    height: auto !important;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .footer-widget {
+    position: relative;
+    bottom: auto;
+    right: auto;
+    margin-top: 20px;
+  }
+}
+
+@media (max-width: 900px) {
+  .footer {
+    height: auto !important;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  
+  .footer-content {
+    max-width: 600px;
+  }
+  
+  .line {
+    flex-direction: column;
+  }
+}
+
 
 </style>
